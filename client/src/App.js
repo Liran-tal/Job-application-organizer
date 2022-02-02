84ac300c56b782b97c626a7c15be24b2c6e467e7
Liran-tal <taliran44@gmail.com>
2022-02-02 14:45:59 +0200

    client basic-table code along before test

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'; 
 import './App.css';
+import Table from './components/basic-table/basic-table.component';
 
 function App() {
   return (
     <div className="App">
       AALLLOOOOO VORLD!!!
+      <Table />
     </div>
   );
 }
---
+++ client/src/components/basic-table/basic-table.component.js
@@ -0,0 +1,54 @@
+import React, { useMemo } from 'react';
+import { useTable } from 'react-table';
+import { COLUMNS } from '../../consts/columns/columns.const';
+import MOCK_DATA from '../../../../MOCK_DATA.json'
+
+function Table() {
+	const columns = useMemo(() => COLUMNS, []);
+	const data = useMemo(() => MOCK_DATA, []);
+
+	const table = useTable({
+		columns,
+		data
+	})
+
+	const {
+		getTableProps,
+		getTableBodyProps,
+		headerGroups,
+		rows,
+		prepareRow,
+	} = table
+
+	return (
+		<table {...getTableProps()}>
+			<thead>
+				{headerGroups.map((headerGroup) => {
+					<tr {...headerGroup.getHeaderGroupProps()}>
+						{headerGroup.headers.map((column) => (
+							<th {...column.getHeaderProps()}>
+								{column.render('Header')}
+							</th>
+						))}
+					</tr>
+				})}
+			</thead>
+			<tbody {...getTableBodyProps()}>
+				{rows.map((row) => {
+					prepareRow(row);
+					return (
+						<tr {...row.getRowProps()}>
+							{row.cells.map((cell) => (
+								<td {...cell.getCellProps()}>
+									{cell.render('Cell')}
+								</td>
+							))}
+						</tr>
+					)
+				})}
+			</tbody>
+		</table>
+	);
+}
+
+export default Table;
