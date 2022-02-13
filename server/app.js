d769cdafc14c65dc170a5d268ff382737a2b5066
Liran-tal <taliran44@gmail.com>
2022-02-13 06:37:51 +0200

    trunc public path in server app.js

--- server/app.js
+++ server/app.js
@@ -5,14 +5,15 @@ const cors = require('cors');
 const usersRouter = require("./routes/users.routes.js");
 const signInRouter = require("./routes/sign_in.routs");
 
-const publicPath = path.join(__dirname, 'client/build');
+const publicPath = path.join(path.dirname(__dirname), 'client/build');
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(express.static(publicPath));
 
 app.get('/', (req, res) => {
-	res.sendFile(path.resolve(publicPath, 'index.html'));
+	// res.sendFile(path.resolve(publicPath, 'index.html'));
+	res.send(path.resolve(publicPath, 'index.html'));
 });
 
 app.use("/api/users", usersRouter);
